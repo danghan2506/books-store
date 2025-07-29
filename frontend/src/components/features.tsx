ed89333276a9f24460727bb9f1c1713b860509aa
danghan2506 <danghan1213@gmail.com>
2025-07-29 21:17:31 +0700

    feat: adds features section, footer and top sales books display

--- frontend/src/components/features.tsx
+++ frontend/src/components/features.tsx
@@ -1,8 +1,43 @@
-import React from 'react'
-
+import {SlidersHorizontal, UserStar,  FolderHeart, CreditCard} from "lucide-react"
 const Features = () => {
   return (
-    <div>Features</div>
+    <section className='mx-auto max-w-[1440px] px-6 lg:px-12 py-16'>
+      <div className="mx-auto max-w-[1440px] px-6 lg:px-12 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 gap-y-12">
+        <div className="flex items-center justify-center flex-col gap-3">
+          <SlidersHorizontal height={44} width={44}/>
+          <div className="flex justify-center items-center flex-col">
+            <h5 className="text-[14px] md:text-[15px] mb-1 font-bold">Advanced Search and Filters</h5>
+            <hr className="w-8 bg-zinc-400 h-1 rounded-full border-none"/>
+          </div>
+          <p className="text-center">Quickly find what you need by title, author, or publishing house.</p>
+        </div>
+        <div className="flex items-center justify-center flex-col gap-3">
+          <UserStar height={44} width={44}/>
+          <div className="flex justify-center items-center flex-col">
+            <h5 className="text-[14px] md:text-[15px] mb-1 font-bold">User Reviews and Ratings</h5>
+            <hr className="w-8 bg-zinc-400 h-1 rounded-full border-none"/>
+          </div>
+          <p className="text-center">User feedback to help you choose better.</p>
+        </div>
+        <div className="flex items-center justify-center flex-col gap-3">
+          <FolderHeart height={44} width={44}/>
+          <div className="flex justify-center items-center flex-col">
+            <h5 className="text-[14px] md:text-[15px] mb-1 font-bold">Wishlist and Favourites</h5>
+            <hr className="w-8 bg-zinc-400 h-1 rounded-full border-none"/>
+          </div>
+          <p className="text-center">Save items you love for later.</p>
+        </div>
+        <div className="flex items-center justify-center flex-col gap-3">
+          <CreditCard height={44} width={44}/>
+          <div className="flex justify-center items-center flex-col">
+            <h5 className="text-[14px] md:text-[15px] mb-1 font-bold">Secure Online Payment</h5>
+            <hr className="w-8 bg-zinc-400 h-1 rounded-full border-none"/>
+          </div>
+          <p className="text-center">Fast, easy, and secure checkout.</p>
+        </div>
+        
+      </div>
+    </section>
   )
 }
 
---
+++ frontend/src/constants/footer-data.ts
@@ -0,0 +1,40 @@
+import { Facebook, Instagram, Twitter, Youtube, Linkedin } from 'lucide-react';
+export const FOOTER_LINKS = [
+    {
+      title: "Learn More",
+      links: [
+        "About Us",
+        "Latest books",
+        "Hot Offers",
+        "Popular books",
+        "FAQ",
+        "Privacy Policy",
+      ],
+    },
+    {
+      title: "Our Community",
+      links: [
+        "Terms and Conditions",
+        "Special Offers",
+        "Customer Reviews",
+      ],
+    },
+  ];
+  export const FOOTER_CONTACT_INFO = {
+    title: "Contact Us",
+    links: [
+      { label: "Contact Number", value: "123-456-7890" },
+      { label: "Email Address", value: "info@bacala.com" },
+    ],
+  };
+export const SOCIALS = {
+  title: "Social",
+  links: [
+    { icon: Facebook, id: "facebook" },
+    { icon: Instagram, id: "instagram" },
+    { icon: Twitter, id: "twitter" },
+    { icon: Youtube, id: "youtube" },
+    { icon: Linkedin, id: "linkedin" },
+  ],
+};
+

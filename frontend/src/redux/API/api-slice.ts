5aaf286cbd0771bb1e0a5f0e11c832550ae05a50
danghan2506 <danghan1213@gmail.com>
2025-08-18 20:59:17 +0700

    fixed: add .env for frontend

--- frontend/src/redux/API/api-slice.ts
+++ frontend/src/redux/API/api-slice.ts
@@ -1,5 +1,6 @@
 import {fetchBaseQuery, createApi} from "@reduxjs/toolkit/query/react"
 import { BASE_URL } from "../features/constants"
+
 const baseQuery = fetchBaseQuery({
   baseUrl: BASE_URL,
   credentials: "include",
--- frontend/src/redux/features/constants.ts
+++ frontend/src/redux/features/constants.ts
@@ -1,4 +1,5 @@
-export const BASE_URL = ""
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000"
+export { BASE_URL }
 export const USERS_URL = "/api/users"
 export const BOOKS_URL = "/api/books"
 export const ORDERS_URL ="/api/orders"

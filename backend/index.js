ef0b98b987bef8c2fd0e50822cc0b16e001277b9
danghan2506 <danghan1213@gmail.com>
2025-08-18 21:13:03 +0700

    fixed: add cors options for pre-flight request handling

--- backend/index.js
+++ backend/index.js
@@ -21,6 +21,7 @@ const corsOptions = {
   allowedHeaders: ['Content-Type', 'Authorization'],
 }
 app.use(cors(corsOptions));
+app.options('*', cors(corsOptions)); 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())

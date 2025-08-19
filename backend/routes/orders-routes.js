8c7d2cec748bc052159ba946a9f9cc92e9c67ecd
danghan2506 <danghan1213@gmail.com>
2025-08-19 10:45:42 +0700

    fixed:wrong routes

--- backend/routes/orders-routes.js
+++ backend/routes/orders-routes.js
@@ -6,7 +6,7 @@ router.route("/").post(authenticate, createOrder).get(authenticate, authorizedAd
 router.route("/my-orders").get(authenticate,getUserOrders)
 router.route("/total-orders").get(authenticate, countTotalOrders)
 router.route("/total-sales").get(authenticate, authorizedAdmin, calculateTotalSales)
-router.route("/total-sales/by-date").get(authenticate, authenticate, calcualteTotalSalesByDate)
+router.route("/total-sales-by-date").get(authenticate, calcualteTotalSalesByDate)
 router.route("/:orderId").get(authenticate, getOrderById)
 router.route("/:orderId/payment").put(authenticate, markOrderAsPaid)
 router.route("/:orderId/delivery").put(authenticate, markOrderAsDelivered)

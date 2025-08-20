be407879c73d9dd925fbcbd0e8ae8684a3295a7d
danghan2506 <danghan1213@gmail.com>
2025-08-20 11:00:30 +0700

    fixed: config index.js file for localhost

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,12 @@ import cors from "cors";
 import { errorHandler } from "./middlewares/error-handler.js";
 const app = express();
 config()
-app.use(cors())
+app.use(cors({
+  origin: process.env.NODE_ENV === "production" 
+    ? process.env.FRONTEND_URL 
+    : "http://localhost:5173",
+  credentials: true
+}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());

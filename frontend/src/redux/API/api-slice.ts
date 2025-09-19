13ed1ec12202646d645044401c485e33cff32dd4
Dang Han <danghan1213@gmail.com>
2025-09-19 10:08:22 +0700

    update env variables for frontend

--- frontend/src/redux/API/api-slice.ts
+++ frontend/src/redux/API/api-slice.ts
@@ -1,13 +1,13 @@
-import {fetchBaseQuery, createApi} from "@reduxjs/toolkit/query/react"
-import { BASE_URL } from "../features/constants"
+import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
+import { BASE_URL } from "../features/constants";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: BASE_URL,
+  baseUrl: BASE_URL || "http://localhost:5000",
   credentials: "include",
-})
+});
 const apiSlice = createApi({
-    baseQuery,
-    tagTypes: ["User", "Book", "Order"],
-    endpoints: () => ({})
-})
-export default apiSlice
+  baseQuery,
+  tagTypes: ["User", "Book", "Order"],
+  endpoints: () => ({}),
+});
+export default apiSlice;
